ed38def5d134ebd0f9dfb97df26c8c1935afc0a6
kenansezginbas <kenansezginbas@hotmail.com>
2023-01-07 22:37:20 +0300

    finish login page

---
+++ mentor/Components/GFButton.swift
@@ -0,0 +1,20 @@
+//
+//  GFButton.swift
+//  mentor
+//
+//  Created by Kenan Sezginbas on 7.01.2023.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ mentor/Screens/Auth/SignUpVC.swift
@@ -0,0 +1,17 @@
+//
+//  SignInVC.swift
+//  mentor
+//
+//  Created by Kenan Sezginbas on 7.01.2023.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemOrange
+    }
+}
