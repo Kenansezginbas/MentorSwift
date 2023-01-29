f3887ddfb371b33d42bbcbb9e0c040fb068e5211
kenansezginbas <kenansezginbas@hotmail.com>
2023-01-29 20:48:50 +0300

    Add TableView

--- mentor/Screens/Menu/HomeVC.swift
+++ mentor/Screens/Menu/HomeVC.swift
@@ -7,11 +7,44 @@
 
 import UIKit
 
-class HomeVC: UIViewController {
+class HomeVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellID", for: indexPath)
+        cell.textLabel?.text = "Kenan"
+        return cell
+    }
+    
+    
+    var customTableView: UITableView = {
+        var tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CellID")
+      
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
 
+    }
+    
+    
+    final func configureUI() {
+        view.addSubview(customTableView)
+        customTableView.frame           = view.bounds
+        customTableView.delegate        = self
+        customTableView.dataSource      = self
+        
+        NSLayoutConstraint.activate([
+            customTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            customTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            customTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                
+        ])
     }
 
 }
